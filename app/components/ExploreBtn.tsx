fa147975964abf9a0c64c4e49da6588347859c62
Gilbert Johnson <johnson220898@gmail.com>
2025-11-06 23:54:42 +0530

    Navbar attach - simple glass navbar

---
+++ app/components/ExploreBtn.tsx
@@ -0,0 +1,14 @@
+'use client'
+import Image from "next/image"
+const ExploreBtn = () => {
+  return (
+    <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={()=>console.log()}>
+        <a href="#events">
+            Explore Events
+            <Image src="/icons/arrow-down.svg" alt="down-arrow" width={20} height={20} className="inline-block ml-2" />
+        </a>
+    </button>
+  )
+}
+
+export default ExploreBtn
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Hello from "./components/Hello";
+import ExploreBtn from "./components/ExploreBtn";
 
 export default function Home() {
 
@@ -6,6 +6,23 @@ export default function Home() {
         <section>
           <h1 className="text-center"> The Hub for every Dev <br/> Event you can miss.</h1>
           <p className="text-center mt-5">Hackathons, Meetups, and conferences, All in here.</p>
+          <ExploreBtn />
+
+          <div className="mt-20 space-y-7">
+            <h3>Featured Events</h3>
+
+            <ul className="events">
+              {[1,2,3,4,5].map((event)=>
+                <li key={event} className="event-card">
+                  <h4>Event Title {event}</h4>
+                  <p className="mt-2 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+                  <span className="mt-3 block text-xs">Date: 20th Aug, 2023</span>
+                </li>
+              )}
+            </ul>
+
+          </div>
+
         </section>
   );
 }
